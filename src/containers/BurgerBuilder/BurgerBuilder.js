2c2d79fd835d0c385603ff66c53b14bc9efbf23f
REED\reed <reed.clarke@rcsofwareservices.com>
2018-04-11 19:48:24 -0400

    initail build

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import Wrapper from '../../hoc/Wrapper/Wrapper';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
-import * as actionTypes from '../../store/actions';
+import * as actionTypes from '../../store/actions/actionTypes';
 
 class BurgerBuilder extends Component {
 
