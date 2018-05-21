5abfd576c5e2657f4d97265e482f840c52530904
REED\reed <reed.clarke@rcsofwareservices.com>
2018-05-21 17:19:43 -0400

    initail build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth'
-import Logout from './containers/Auth/Logout/Logout';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth'
+import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
 
 class App extends Component {
+    componentDidMount () {
+        this.props.onAutoSignup();
+    }
+
   render() {
     return (
       <div>
@@ -26,4 +32,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToPorps = dispatch => {
+    return {
+        onAutoSignup: () => dispatch(actions.authCheckState())
+    }
+};
+
+export default withRouter(connect(null, mapDispatchToPorps)(App));
